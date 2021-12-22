70b137c7c52359f7a4ccea25365e02c6fdb426e3
LiorBreitman8234 <liorbreitman321@gmail.com>
2021-12-22 19:48:27 +0200

    pictures for test

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        Ex2.getGrapgAlgo(args[0]);
-        Ex2.getGrapg(args[0]);
-        Ex2.runGUI(args[0]);
+        String file = "/home/bravo8234/LargeConnectedGraphs/10000Nodes.json";
+        GraphAlgorithms GA = new GraphAlgorithms();
+        long start_time = System.currentTimeMillis();
+        GA.load(file);
+        long end_time = System.currentTimeMillis();
+        System.out.println("time to load " + file + " : " + (end_time-start_time) + " MS");
+        start_time = System.currentTimeMillis();
+        GA.save("test.json");
+        end_time = System.currentTimeMillis();
+        System.out.println("time to save" + file + " : " + (end_time-start_time) + " MS");
     }
 }
